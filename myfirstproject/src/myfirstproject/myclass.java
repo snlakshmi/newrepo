635d9a7e49e2328acd6b3e8a51ee3e51c68e081f
Raghu Nandan <sprasad@Lenovo-PC.(none)>
2016-07-12 11:25:09 +0530

    my first commit

---
+++ myfirstproject/src/myfirstproject/myclass.java
@@ -0,0 +1,20 @@
+package myfirstproject;
+import java.util.Scanner;
+public class myclass {
+
+	public static void main(String[] args) {
+		int a;
+		int b;
+		int c;
+		Scanner in = new Scanner(System.in);
+		System.out.println("enter any two integers");
+		a = in.nextInt();
+		b = in.nextInt();
+		c = a+b;
+		System.out.println("sum =" +c);
+		
+		
+		
+	}
+
+}
